fefbef0bbdc1a57724fa31faf8c2991a072b7ebf
emir <kodmanyagha@gmail.com>
2020-03-21 14:26:47 +0300

    minor changes

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <a href="gulizar.php">Gülizar Açıkgöz</a>
 <a href="hasim.php">Haşim Açıkgöz</a>
 <a href="erdener.php">Erdener KILIÇ</a>
+<a href="bayramcan.php?user=bayram">Bayram Can Aykul</a>
 <!--
 Süper
 -->
